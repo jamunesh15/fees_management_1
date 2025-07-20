ed6933d238c0acb8977b13eaa41792625dc963bb
jamunesh15 <jsheta15@gmail.com>
2025-07-20 17:26:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ dotenv.config({ debug: true });
 const app = express();
 
 app.use(cors({
- origin: 'https://fees-management-1-ui.onrender.com',
+ origin: true,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   
@@ -65,6 +65,15 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connecte
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/fees', feeRoutes);
+
+//default
+app.get("/" , (req,res) => {
+    return res.status(200).json({
+        success:true,
+        message:"Your Server is Up and Running"
+    })
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
